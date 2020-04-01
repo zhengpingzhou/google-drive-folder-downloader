e4405f6c4098b778b82dbf453f2818c4263b8a94
Zhengping Zhou <zpzhou@stanford.edu>
2020-03-31 17:42:09 -0700

    support cli download with local browser auth

--- download.py
+++ download.py
@@ -19,8 +19,21 @@ def main():
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
-            creds = flow.run_local_server(port=1337)
+            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES, redirect_uri='urn:ietf:wg:oauth:2.0:oob')
+            # creds = flow.run_local_server(port=1337)
+            
+            # Tell the user to go to the authorization URL.
+            auth_url, _ = flow.authorization_url(prompt='consent')
+
+            print('Please go to this URL: {}'.format(auth_url))
+
+            # The user will get an authorization code. This code is used to get the
+            # access token.
+            code = input('Enter the authorization code: ')
+            flow.fetch_token(code=code)
+
+            creds = flow.credentials
+
         with open('token.pickle', 'wb') as token:
             pickle.dump(creds, token, protocol=0)
     service = build('drive', 'v3', credentials=creds)
